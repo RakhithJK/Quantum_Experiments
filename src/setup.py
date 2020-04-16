d3ae1014c832bd8e89c1f77eb3c401e0bd6ef35b
Steinkirch <steinkirch@zapatacomputing.com>
2020-04-16 11:56:58 -0700

    add pakg

--- src/setup.py
+++ src/setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ),
-    py_modules=['doordash']
+    py_modules=['doordash'],
     install_requires = [
        "urllib3",
        "requests",
