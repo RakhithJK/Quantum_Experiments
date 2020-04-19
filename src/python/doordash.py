4f1c56fc7473be42cdd4f4341e0fe458872ae150
Steinkirch <steinkirch@zapatacomputing.com>
2020-04-18 17:15:25 -0700

    clean up

--- src/python/doordash.py
+++ src/python/doordash.py
@@ -2,26 +2,15 @@
 
 import json
 import requests
-#import urlparse
 
 
 def query_doordash_by_location(baseurl=None, latitude=None, longitude=None):
-    """Search for restaurants around a location.
-    
-    Args:
-        baseurl (str): the base url for doordash API.
-        latitude (str): latitude to query.
-        longitude (str): longitude to query.
-        
-    Returns:
-        dict: a dictionary containing found restaurants.
-    """
+    """Hackathon exercise """
 
     baseurl = "https://www.doordash.com"
     latitude = "39.596371"
     longitude = "-119.777603"
 
-   # url = urlparse.urljoin(baseurl, 'api/v2/restaurant/?lat={0}&lng={1}'.format(latitude, longitude))
     url = "https://www.doordash.com/api/v2/restaurant/?lat={0}&lng={1}".format(latitude, longitude) 
     response = requests.get(url)
 
